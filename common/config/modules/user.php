045127a2f9a6c7c1a47f79ee5d97a9b95a7b8a8f
Surakit Choodet <kuakling@gmail.com>
2017-03-26 07:50:28 +0000

    Install andatech/yii2-user

---
+++ common/config/modules/user.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'class' => 'anda\user\Module',
+    'loginBy' => 'db', //db or ldap
+    'userUploadDir' => '@uploads', //real path
+    'userUploadUrl' => '/uploads', //url path
+    'userUploadPath' => 'user', //path after upload directory
+    'admins' => ['admin', 'root'] //list username for manage users
+];
